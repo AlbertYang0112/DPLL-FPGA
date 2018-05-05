eb12944a028ddb05196bc3e7e5e5d105c8bd8838
invy <1004018919@qq.com>
2018-05-05 23:14:21 +0800

    Add the info.

---
+++ DPLL.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:05:55 05/05/2018 
+// Design Name:    
+// Module Name:    DPLL 
+// Project Name:   DPLL-FPGA
+// Target Devices: XC6SLX9-2FTG256
+// Tool versions: 
+// Description:     The digital phase lock loop.
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module DPLL(
+    input baseClockInput,
+    input oscInput,
+    output dpllOutput
+    );
+
+
+endmodule
