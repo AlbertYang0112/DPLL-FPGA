f7e2b558a27b97a16d118bfa2e37426147759b35
Albert Young <invy0112@gmail.com>
2018-05-06 02:15:29 +0800

    The XOR phase detector.

---
+++ PhaseDetector.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    01:08:06 05/06/2018 
+// Design Name: 
+// Module Name:    PhaseDetector 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module PhaseDetector(
+    input inputSigA,
+    input inputSigB,
+    output errSig
+    );
+
+    errSig = inputSigA ^ inputSigB;
+
+endmodule
